c9b6c712356e558b493edfb4e411290e1efded2b
Mustafa Iswardana <100180340+MTF51@users.noreply.github.com>
2023-03-15 07:58:51 +0700

    Add files via upload

---
+++ Praktikum3/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/** 
+ * File		: Poligon.java/8/Maret/2023
+ * Penulis	: Mustafa iswardana
+ * Deskripsi	: Representasi dasar dari objjek poligon (segi banyak)
+ */
+
+package org.poligon;
+	
+public class Poligon{
+	protected int jumlahSisi;
+		
+	public int getHumlahSisi(){
+		return this.jumlahSisi;	
+	}
+}
